4f3e630fc4ed4479fd9d1982aa35f55b1adb82bd
adedejiosvaldo <adedejiosvaldo@gmail.com>
2025-10-12 22:32:29 +0100

    feat: postgres db connection linkage

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,16 +13,18 @@ func main() {
 	cfg := config{
 		address: env.GetString("ADDR", ":8080"),
 		dbConfig: dbConfig{
-			address:      env.GetString("DB_ADDR", "postgres://user:adminpassword@localhost/social?sslmode=disable"),
+			address:      env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
-			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15min")}}
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m")}}
 
 	db, err := db.New(cfg.dbConfig.address, cfg.dbConfig.maxOpenConns, cfg.dbConfig.maxIdleConns, cfg.dbConfig.maxIdleTime)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	defer db.Close()
+	log.Println("Database connection established")
 	store := store.NewPostgresStorage(db)
 
 	app := &application{config: cfg, store: store}
