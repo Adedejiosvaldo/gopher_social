d82ee576e21517e7d0e6059fe3712c0366677932
adedejiosvaldo <adedejiosvaldo@gmail.com>
2025-10-23 20:44:49 +0100

    feat: creating user feed post

--- internal/store/post.go
+++ internal/store/post.go
@@ -26,14 +26,12 @@ type Post struct {
 func (s *PostStore) Create(ctx context.Context, post *Post) error {
 	query := `
 	INSERT INTO posts (content, title, user_id,tags)
-	VALUES ($1, $2, $3, $4) RETURNING id,created_at, updated_at
+	VALUES ($1, $2, $3, $4) RETURNING id, created_at, updated_at
 
 	`
 
 	err := s.db.QueryRowContext(
-		ctx, query, post.Content, post.Title, post.UserID,
-		pq.Array(post.Tags)).Scan(&post.ID, &post.CreatedAt, &post.UpdatedAt,
-		post.Tags)
+		ctx, query, post.Content, post.Title, post.UserID, pq.Array(post.Tags)).Scan(&post.ID, &post.CreatedAt, &post.UpdatedAt)
 
 	if err != nil {
 		return err
